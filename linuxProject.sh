bdfeba5b9b08415dbf3b5adc7f05524528d7354a
Alex Assante <maa0110@cmpt251.westminstercollege.edu>
2019-12-10 23:13:14 -0700

    first function

--- linuxProject.sh
+++ linuxProject.sh
@@ -3,4 +3,24 @@
 # Author: Alex Assante
 # Linux Project
 
+arg_num=$#
+arg1=$1
+arg2=$2
+arg3=$3
+arg4=$4
+args=$@
 
+initialize() {
+	check_directories "todo" "todo_completed"
+	check_stdin
+	count_incomplete
+	count_completed
+
+	if [ $arg_num -ne 0 ]; then 
+		menu_mode=0
+		use_args
+	else
+		menu_mode=1
+		display_menu
+	fi
+}
